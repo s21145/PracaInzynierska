202f02ddf63b14861d5c8a4fdaaabe5876c8d2ac
Maciej Krawczyk <s20898@pjwstk.edu.pl>
2024-08-05 21:24:41 +0200

    Zmiana listowania postów na jeden na wiersz nie zależnie od szerokości widoku

--- pracainzynierska-frontend/src/pages/Posts/PostItem.js
+++ pracainzynierska-frontend/src/pages/Posts/PostItem.js
@@ -7,37 +7,36 @@ const MAX_LENGTH = 230;
 function PostItem({ idUserOwner, title, content, userLogin, postId }) {
   return (
     <div className="posts__item__wrapper">
-        <li className="posts__item">
-          <div className="posts__item__container">
-            <div className="posts__item__postedby">
-              <h4>Posted by: {userLogin}</h4>
-              {/*<i className="fa-solid fa-ellipsis fa-xl"></i>   -- 3 dots*/}
-            </div>
-            <div className="posts__item__title">
-              <h3>{title}</h3>
-            </div>
-            {content.length > MAX_LENGTH ? (
-              <div className="posts__item__content">
-                {`${content.substring(0, MAX_LENGTH)}...`} Read more
-              </div>
-            ) : (
-              <div className="posts__item__content">{content}</div>
-            )}
-            <div className="posts__item__footer">
-              <div className="posts__item__comments">
-                <Link to={`/posts/${postId}`} className="link-wrapper">
-                  <h2>
-                    <i className="fa-solid fa-comments"></i> Comments
-                  </h2>
-                </Link>
-              </div>
-              <div className="posts__item__likes">
-                <h2>3 <i class="fa-solid fa-heart"></i></h2>
-              </div>
-            </div>
-            
+      <li className="posts__item">
+        <div className="posts__item__container">
+          <div className="posts__item__postedby">
+            <h4>Posted by: {userLogin}</h4>
+            {/*<i className="fa-solid fa-ellipsis fa-xl"></i>   -- 3 dots*/}
           </div>
-        </li>
+          <div className="posts__item__title">
+            <h3>{title}</h3>
+          </div>
+          {content.length > MAX_LENGTH ? (
+            <div className="posts__item__content">
+              {`${content.substring(0, MAX_LENGTH)}...`} Read more
+            </div>
+          ) : (
+            <div className="posts__item__content">{content}</div>
+          )}
+          <div className="posts__item__footer">
+            <div className="posts__item__comments">
+              <Link to={`/posts/${postId}`} className="link-wrapper">
+                <h2>
+                  <i className="fa-solid fa-comments"></i> Comments
+                </h2>
+              </Link>
+            </div>
+            <div className="posts__item__likes">
+              <h2>3 <i className="fa-solid fa-heart"></i></h2>
+            </div>
+          </div>
+        </div>
+      </li>
     </div>
   );
 }
